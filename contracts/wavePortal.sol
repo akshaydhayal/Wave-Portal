7d781f0cbe169b46e3b9bf9b2ad4cc09eee5cfd9
Akshay Dhayal <akshaydhayalkd99@gmail.com>
2021-10-27 13:21:17 +0000

    Compile& Deploy smart contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -1,5 +1,6 @@
-pragma solididy ^0.8.0;
-import"hardhat/console.log";
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import"hardhat/console.sol";
 
 contract WavePortal{
   constructor(){
